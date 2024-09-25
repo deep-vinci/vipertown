d63ced41457c5f14b6abbc84ac65668d96e09977
Deepak <deepakworkpc@gmail.com>
2024-09-25 22:26:00 +0530

    feat: start viper body from cell 1 only

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ const paintFood = () => {
 }
 
 paintFood();
-viper = [generateArrayOfTwoRandomInt(), generateArrayOfTwoRandomInt()];
+viper = [generateArrayOfTwoRandomInt()];
 
 setInterval(() => {
     
@@ -154,5 +154,5 @@ document.addEventListener("keydown", (event) => {
 })
 
 restartGame.addEventListener("click", () => {
-    location.reload();
+    location.reload()
 })
