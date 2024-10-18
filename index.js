3cae70baf19756c51f78621474f4415780b337dd
Deepak <deepakworkpc@gmail.com>
2024-10-18 18:34:46 +0530

    feat: add random position to viper start

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const gameBoard = document.querySelector(".game-board");
 const score = document.querySelector(".score span");
 
 let currentInput = "w";
-let viper = [[9,7], [9,8], [9,9]];
+let viper;
 let viperFood;
 let width = 20;
 let lastLocation = [0, 0];
+
 let colorScheme = {
     viper: "#708742",
     board: "#e2f3c1",
     food: "#f25555"
 }
 
+gameBoard.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
+gameBoard.style.gridTemplateRows = `repeat(${width}, 1fr)`;
 for(let i = 0; i < width; i++) {
     for(let j = 0; j < width; j++) {
         let cell = document.createElement("div");
@@ -60,6 +63,7 @@ const paintFood = () => {
 }
 
 paintFood();
+viper = [foodLocation(), foodLocation()];
 setInterval(() => {
     
     removeViperTrail(viper);
@@ -88,11 +92,11 @@ setInterval(() => {
             ...viper,
             lastLocation
         ]
-        score.textContent = `${Number(score.textContent) += 1}`;
         // console.log(Number(score.textContent) + 1)
-     }
+        score.textContent = `${Number(score.textContent) + 1}`;
+    }
     
-    
+
     if (currentInput == "w") {
 
         viper = [ 
@@ -134,16 +138,16 @@ setInterval(() => {
     lastLocation = viper[viper.length - 1];
     console.log(viper)
 
-}, 200)
+}, 300)
 
 document.addEventListener("keydown", (event) => {
-    if (event.key == "w") {
+    if (event.key == "w" || event.key == "ArrowUp") {
         currentInput = "w";
-    } else if (event.key == "a") {
+    } else if (event.key == "a" || event.key == "ArrowLeft") {
         currentInput = "a";
-    } else if (event.key == "s") {
+    } else if (event.key == "s" || event.key == "ArrowDown") {
         currentInput = "s";
-    } else if (event.key == "d") {
+    } else if (event.key == "d" || event.key == "ArrowRight") {
         currentInput = "d";
     }
 })
